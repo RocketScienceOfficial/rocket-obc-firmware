d6842599dc8bc04d497f0a23235ce5e74fd488ad
Filipeak <filippo.gawlik@gmail.com>
2022-06-06 19:03:45 +0200

    Github workflow test

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@
 
 cd `dirname $0`/..
 
-sudo apt install node cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib build-essential
+sudo apt install nodejs cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib build-essential
 
 cd server
 npm install
