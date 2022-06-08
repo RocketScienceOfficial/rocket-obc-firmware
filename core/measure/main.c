81899060b1edcd961888596c98f80998182270b3
Filip Gawlik <filippo.gawlik@gmail.com>
2022-06-08 21:19:58 +0200

    Test

--- core/measure/main.c
+++ core/measure/main.c
@@ -68,7 +68,7 @@ void initialize()
     loraInit(&s_LoraData, &loraPinout);
 
     s_LoraData.pinout = loraPinout;
-    s_LoraData.txPower = 20;
+    s_LoraData.txPower = 17;
 
     loraBegin(&s_LoraData, SX1278_FREQ_HZ);
 
