c99b847e7dcfc5e46e9c3938201c9b40a83ef75b
Filip Gawlik <filippo.gawlik@gmail.com>
2024-02-01 18:03:25 +0100

    Simple SM & IGN tests

---
+++ tests/ign/ign_test.c
@@ -0,0 +1,19 @@
+#include "tests/test_framework.h"
+#include "modules/ign/ign_controller.h"
+
+int main()
+{
+    TEST_START();
+
+    TEST_START_SECTION("Check pins");
+    {
+    }
+    TEST_END_SECTION();
+
+    TEST_START_SECTION("Test igniters");
+    {
+    }
+    TEST_END_SECTION();
+
+    TEST_END();
+}
